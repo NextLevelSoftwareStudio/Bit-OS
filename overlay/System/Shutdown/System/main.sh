b60cb663b640a07f60826a07b19977b0c7cabf35
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-05-01 23:09:02 +0000

    .

--- overlay/System/Shutdown/System/main.sh
+++ overlay/System/Shutdown/System/main.sh
@@ -1,3 +1,4 @@
 #Updating the environment variables and sourcing the profile to apply changes
 sudo env-update
-source /etc/profile
+# Adding the local service to the default runlevel to ensure it starts on boot
+rc-update add local default
