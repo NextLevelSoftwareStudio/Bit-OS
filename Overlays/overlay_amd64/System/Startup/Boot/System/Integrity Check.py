6e048b573c220837d73a9809275fe253223743fc
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-05-17 01:03:56 +0100

    :)

--- Overlays/overlay_amd64/System/Startup/Boot/System/Integrity Check.py
+++ Overlays/overlay_amd64/System/Startup/Boot/System/Integrity Check.py
@@ -1,7 +1,18 @@
 from pathlib import Path
+from datetime import datetime, timezone
 systemfolder = Path("/System")
 usersfolder = Path("/Users")
-logfile = systemfolder / "var" / "log" / "IntegrityCheck.log"
-systemfolder.mkdir(parents=True, exist_ok=True)
-usersfolder.mkdir(parents=True, exist_ok=True)
-logfile.touch(exist_ok=True)
+logfile = systemfolder / "var" / "log" / "IntegrityCheck.jonl.log"
+
+def log(msg):
+    with open(logfile, "a") as f:
+        f.write(str(msg))
+
+if systemfolder.exists() is False:
+    systemfolder.mkdir()
+    log(r"{timestamp:}")
+if usersfolder.exists() is False:
+    usersfolder.mkdir()
+if logfile.exists() is False:
+    logfile.touch()
+print(datetime.now(timezone.utc).strftime("%d/%m/%Y %H:%M:%S"))
