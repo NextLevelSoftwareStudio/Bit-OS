8ddca69e0a71b2010d96102435a3dca0365f5088
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-05-17 19:41:38 +0100

    .

--- installer.py
+++ installer.py
@@ -1,4 +1,5 @@
-import locale, platform, sys, os
+import locale, platform, sys, os, subprocess
+from pathlib import Path
 # lang = 'pt_PT', encoding = 'UTF-8'
 encoding = locale.getencoding()
 lang, _ = locale.getlocale()
@@ -7,9 +8,11 @@ portuguese = ["português", "portuguese", "portugiesisch"]
 english = ["inglês", "english", "englisch"]
 german = ["alemão", "german", "deutsch"]
 filesystems = ["ext4", "brtfs", "gpfs"]
+
 if encoding != "UTF-8":
     print("Your system's encoding is not UTF-8. Please change it to UTF-8 to continue.")
     sys.exit(1)
+
 caminho_modelo = '/proc/device-tree/model'
 if os.path.exists(caminho_modelo): # Verifica se o arquivo existe (sistemas x86 comuns não têm essa árvore de dispositivos)
     try:
@@ -23,33 +26,46 @@ if os.path.exists(caminho_modelo): # Verifica se o arquivo existe (sistemas x86 
     except Exception:
         print("Could not read the device model.")
 
-# Installation for amd64 architecture
-if cpu_architecture == "x86_64":
-    while True:
-        if encoding == "UTF-8":
-            if lang == "pt_PT":
-                print("Idioma detectado: Português")
-                entrada = input("Que idioma deseja definir com padrão? ").lower()
-                if entrada == "":
-                    entrada = "portuguese"
-                break
-            elif lang == "en_US":
-                print("Language detected: English")
-                entrada = input("What language would you like to set as default? ").lower()
-                if entrada == "":
-                    entrada = "english"
-                break
-            elif lang == "de_DE":
-                print("Sprache erkannt: Deutsch")
-                entrada = input("Welche Sprache möchten Sie als Standard festlegen? ").lower()
-                if entrada == "":
-                    entrada = "german"
-                break
-    if entrada in portuguese:
-        
-    elif entrada in english:
-        
-    elif entrada in german:
-        
+localegenfile = Path('/etc/locale.gen')
+def language(data):
+    with open(localegenfile, 'a') as f:
+        f.write(data)
+    subprocess.run(['locale-gen'])
+
+while True:
+    with open(localegenfile, 'w') as f: # apagando o conteúdo do arquivo para apagar locales antigos
+        pass
+    if lang == "pt_PT":
+        print("Idioma detectado: Português")
+        entrada = input("Que idioma deseja definir com padrão? ").lower()
+        if entrada == "":
+            entrada = "portuguese"
+        language("pt_PT.UTF-8 UTF-8")
+        break
+    elif lang == "en_US":
+        print("Language detected: English")
+        entrada = input("What language would you like to set as default? ").lower()
+        if entrada == "":
+            entrada = "english"
+        language("en_US.UTF-8 UTF-8")
+        break
+    elif lang == "de_DE":
+        print("Sprache erkannt: Deutsch")
+        entrada = input("Welche Sprache möchten Sie als Standard festlegen? ").lower()
+        if entrada == "":
+            entrada = "german"
+        language("de_DE.UTF-8 UTF-8")
+        break
     else:
-        
+        print("Language doesn'y exists or ins't available.")
+            
+
+# wget use flags configuration
+wgetuseflagsfile = Path('/etc/portage/package.use/net-misc/wget')
+with open(wgetuseflagsfile, 'w') as f:
+    f.write('net-misc/wget ssl idn nls verify-sig metalink libproxy cookie-check')
+try:
+    subprocess.run(['emerge', '--ask', 'net-misc/wget'], check=True)
+
+except subprocess.CalledProcessError:
+    print("Error occurred while installing wget.")
